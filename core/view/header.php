110d0aa654afbc064ef64a1d00b7a03a33201eaa
ItalianPepper <Oraion335@gmail.com>
2017-01-23 16:57:48 +0100

    fix header

--- core/view/header.php
+++ core/view/header.php
@@ -17,7 +17,7 @@
                     <button type="button" class="navbar-toggle" data-toggle="collapse"
                             data-target="#bs-example-navbar-collapse-1">
                         <span class="sr-only">Toggle navigation</span> Menu <i
-                                class="fa fa-bars"></i>
+                            class="fa fa-bars"></i>
                     </button>
                 </div>
             </div>
@@ -26,26 +26,30 @@
 
             <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12" style="margin-top: 0.5%;margin-bottom:2%;">
                 <div class="left">
-                    <div class="input-group">
-                        <div class="input-group-btn search-panel">
-                            <button type="button" class="btn btn-default dropdown-toggle"
-                                    data-toggle="dropdown">
-                                <span id="search_concept">Appunti</span> <span class="caret"></span>
-                            </button>
-                            <ul id="list-option" class="dropdown-menu" role="menu">
-                                <li><a>Appunti</a></li>
-                                <li><a>Annunci</a></li>
-                                <li><a>Tag</a></li>
-                            </ul>
-                        </div>
-                        <input type="hidden" name="search_param" value="appunti" id="search_param">
-                        <input type="text" name="user_param" class="form-control"  placeholder="Search">
-                        <span class="input-group-btn">
-								<button style="width: 100%" class="btn btn-default" type="button">
+                    <form action="<?php echo DOMINIO_SITO;?>/search" method="post">
+                        <div class="input-group">
+                            <div class="input-group-btn search-panel">
+                                <button type="button" class="btn btn-default dropdown-toggle"
+                                        data-toggle="dropdown">
+                                    <span id="search_concept">Appunti</span> <span class="caret"></span>
+                                </button>
+                                <ul id="list-option" class="dropdown-menu" role="menu">
+                                    <li><a>Appunti</a></li>
+                                    <li><a>Annunci</a></li>
+                                    <li><a>Tag</a></li>
+                                </ul>
+                            </div>
+
+                            <input type="hidden" name="search_param" value="appunti" id="search_param">
+                            <input type="text" name="user_param" class="form-control"  placeholder="Search">
+
+                            <span class="input-group-btn">
+								<button type="submit" style="width: 100%" class="btn btn-default" type="button">
 									<span class="glyphicon glyphicon-search"></span>
 								</button>
 							</span>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
 
@@ -58,7 +62,7 @@
                         <li class="page-scroll"><a href="<?php echo DOMINIO_SITO . "/listaAnnunci"?>">Annunci</a></li>
                         <li class="page-scroll"><a href="<?php echo DOMINIO_SITO . "/categorie"?>">Appunti</a></li>
                         <?php if(isset($_SESSION["user"])){?>
-                        <li class="page-scroll"><a href="<?php echo DOMINIO_SITO ."/profiloUtente"?>">Profilo</a></li>
+                            <li class="page-scroll"><a href="<?php echo DOMINIO_SITO ."/profiloUtente"?>">Profilo</a></li>
                         <?php } ?>
                         <li class="page-scroll"><a href="#">Help</a></li>
                         <li><a href="<?php echo DOMINIO_SITO . "/login"?>">Login</a></li>
