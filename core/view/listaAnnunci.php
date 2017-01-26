b7a90ce06351de66c32c09c891f5857c6ea5f5f8
ItalianPepper <Oraion335@gmail.com>
2017-01-26 15:43:56 +0100

    fix liste

--- core/view/listaAnnunci.php
+++ core/view/listaAnnunci.php
@@ -20,12 +20,16 @@
 <section id="contact">
     <div class="container" style="margin-top: 10%;">
         <div class="row">
-            <div class="col-lg-12 text-center">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
                 <h2>Lista Annunci </h2>
                 <hr class="star-primary">
-                <a href="<?php echo DOMINIO_SITO;?>/inserisciAnnuncio">
+
+                <?php if(isset($_SESSION["user"])){ ?>
+                    <a href="<?php echo DOMINIO_SITO;?>/inserisciAnnuncio">
                     <button type="submit" class="btn btn-success btn-lg" style="float: right;">Aggiungi nuovo Annuncio +</button>
                 </a>
+                <?php } ?>
+
             </div>
         </div>
 
@@ -35,37 +39,25 @@
             ?>
 
             <div class="row">
-                <div class="col-lg-8 col-lg-offset-2">
-
-                    <div class="row" style="  position:relative;
-    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
-       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
-            box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;">
-                        <div class="form-group col-xs-12" style="float: right; margin-top: 2%">
-                            <P><b> Titolo:</b>&nbsp <?php echo $annunci[$i]->getTitolo()?></P>
-                            <P><b> Descrizione: </b>&nbsp <?php echo $annunci[$i]->getDescrizione()?> .</P>
-                            <P><b> Tag:</b>&nbsp Affitto &nbsp Camera &nbsp Singola </P>
-                            <P><b> Utente:</b>&nbsp <?php ?> </P>
-                            <P><b> Data:</b>&nbsp <?php echo $annunci[$i]->getDataDiCaricamento();?> </P>
-                            <button type="submit" class="btn btn-success btn-lg" style="float: right;">Leggi
-                            </button>
-                        </div>
-                    </div>
+                <div class="col-lg-12 col-md 12- col-sm-12 col-xs-12">
+                    <p><b> Titolo:</b>&nbsp <?php echo $annunci[$i]->getTitolo();?></p>
+                    <p><b> Descrizione: </b>&nbsp <?php echo $annunci[$i]->getDescrizione();?></p>
+                    <p><b> Tag:</b>&nbsp Affitto &nbsp Camera &nbsp Singola</p>
+                    <P><b> Utente:</b>&nbsp <?php ?> </P>
+                    <p><b> Data:</b>&nbsp <?php echo $annunci[$i]->getDataDiCaricamento();?></p>
                 </div>
             </div>
 
             <?php
         }
         ?>
-
-
     </div>
 </section>
 
 
 
 <!-- Footer -->
-<<?php  include_once VIEW_DIR . "footer.php"; ?>
+<?php  include_once VIEW_DIR . "footer.php"; ?>
 
 <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
 <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
--- core/view/listaAppunti.php
+++ core/view/listaAppunti.php
@@ -19,43 +19,36 @@
 <section id="contact">
     <div class="container" style="margin-top: 10%;">
         <div class="row">
-            <div class="col-lg-12 text-center">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
                 <h2>Lista appunti </h2>
                 <hr class="star-primary">
+                <?php if(isset($_SESSION["user"])){ ?>
+                    <a href="<?php echo DOMINIO_SITO;?>/inserisciAppunti">
+                        <button type="submit" class="btn btn-success btn-lg" style="float: right;">Aggiungi nuovi Appunti +</button>
+                    </a>
+                <?php } ?>
             </div>
         </div>
 
 
         <?php
-
         for ($i = 0; $i < count($appunti); $i++) {
             ?>
 
             <div class="row">
-                <div class="col-lg-8 col-lg-offset-2">
-                        <br>
-                        <div class="row" style="  position:relative;
-    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
-       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
-            box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;">
-                            <div class="form-group col-xs-12" style="float: right; margin-top: 2%">
-                                <P><b> Titolo:</b>&nbsp <?php echo $appunti[$i]->getNome(); ?></P>
-                                <P><b> Descrizione: </b>&nbsp <?php echo $appunti[$i]->getDescrizione(); ?> .</P>
-                                <P><b> Tag:</b>&nbsp Affitto &nbsp Camera &nbsp Singola </P>
-                                <P><b> Utente:</b>&nbsp <?php ?> </P>
-                                <P><b> Data:</b>&nbsp <?php echo $appunti[$i]->getDataDiCaricamento(); ?> </P>
-                                <a href="<?php echo DOMINIO_SITO;?>/scaricaAppunti/<?php echo $appunti[$i]->getKeyFile(); ?>">
-                                <button class="btn btn-success btn-lg" style="float: right; margin-left: 1%;">Download
-                                <i class="fa fa-download"></i></button>
-                                </a>
-                                <a href="<?php echo DOMINIO_SITO;?>/visualizzaFile/<?php echo $appunti[$i]->getKeyFile(); ?>"><button class="btn btn-success btn-lg" style="float: right;">Leggi
-                                <i class="fa fa-file-pdf-o"></i></button></a>
-
-                            </div>
-                        </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <p><b> Titolo:</b>&nbsp <?php echo $appunti[$i]->getNome(); ?></p>
+                    <p><b> Descrizione: </b>&nbsp <?php echo $appunti[$i]->getDescrizione(); ?> .</p>
+                    <p><b> Tag:</b>&nbsp Affitto &nbsp Camera &nbsp Singola </p>
+                    <p><b> Utente:</b>&nbsp <?php ?> </p>
+                    <p><b> Data:</b>&nbsp <?php echo $appunti[$i]->getDataDiCaricamento(); ?> </p>
+                    <a href="<?php echo DOMINIO_SITO;?>/scaricaAppunti/<?php echo $appunti[$i]->getKeyFile(); ?>">
+                        <button class="btn btn-success btn-lg" style="float: right; margin-left: 1%;">Download
+                            <i class="fa fa-download"></i></button>
+                    </a>
+                    <a href="<?php echo DOMINIO_SITO;?>/visualizzaFile/<?php echo $appunti[$i]->getKeyFile(); ?>"><button class="btn btn-success btn-lg" style="float: right;">Leggi<i class="fa fa-file-pdf-o"></i></button></a>
                 </div>
             </div>
-
             <?php
         }
         ?>
